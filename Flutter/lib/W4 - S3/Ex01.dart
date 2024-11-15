70694c973bdda290fe257fe47eddc0368163b8a1
Blaze <sokthy99999@gmail.com>
2024-11-15 17:23:32 +0700

    Flutter

--- Flutter/lib/W4 - S3/Ex01.dart
+++ Flutter/lib/W4 - S3/Ex01.dart
@@ -1,1 +1,58 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Custom buttons"),
+        ),
+        body: Center(
+          child: SelectableButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class SelectableButton extends StatefulWidget {
+  @override
+  _SelectableButtonState createState() => _SelectableButtonState();
+}
+
+class _SelectableButtonState extends State<SelectableButton> {
+  bool _isSelected = false;
+
+  void _toggleSelection() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  String get _buttonText => _isSelected ? "Selected" : "Not Selected";
+  Color get _textColor => _isSelected ? Colors.white : Colors.black;
+  Color get _backgroundColor => _isSelected ? Colors.blue[500]! : Colors.blue[50]!;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: _toggleSelection,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: _backgroundColor,
+        ),
+        child: Center(
+          child: Text(
+            _buttonText,
+            style: TextStyle(color: _textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Flutter/lib/W4 - S3/Ex03.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+List<String> images = [
+  "assets/w4-s2/bird.jpg",
+  "assets/w4-s2/insect.jpg",
+  "assets/w4-s2/girl.jpg",
+  "assets/w4-s2/man.jpg",
+];
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Hide the debug banner
+      home: ImageGallery(),
+    );
+  }
+}
+
+class ImageGallery extends StatefulWidget {
+  @override
+  _ImageGalleryState createState() => _ImageGalleryState();
+}
+
+class _ImageGalleryState extends State<ImageGallery> {
+  int _currentIndex = 0;
+
+  void _showNextImage() {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % images.length;
+    });
+  }
+
+  void _showPreviousImage() {
+    setState(() {
+      _currentIndex = (_currentIndex - 1 + images.length) % images.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        backgroundColor: Colors.green[400],
+        title: const Text('Image viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.navigate_before),
+            tooltip: 'Go to the previous image',
+            onPressed: _showPreviousImage,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
+            child: IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to the next image',
+              onPressed: _showNextImage,
+            ),
+          ),
+        ],
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Image.asset(images[_currentIndex]),
+      ),
+    );
+  }
+}
